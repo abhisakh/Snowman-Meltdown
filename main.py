5460f99398004dd951cc35584b9d1fc5710a47f0
Abhisakh Sarma <abhisakh@gmail.com>
2025-09-12 22:59:46 +0200

    user wish(y/n) loop incorporated in main

--- main.py
+++ main.py
@@ -2,5 +2,20 @@
 
 from game_logic import play_game
 
+def main():
+    while True:
+        play_game()
+
+        while True:  # Nested loop to validate the user input for replay
+            play_again = input("Do you want to play again? (y/n): ").lower().strip()
+            if play_again == 'y':
+                break  # Exit inner loop and restart game
+            elif play_again == 'n':
+                print("Good Bye! Thanks for playing Snowman Meltdown!")
+                return  # Exit the program
+            else:
+                print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+
 if __name__ == "__main__":
-    play_game()
+    main()
+
