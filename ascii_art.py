3db0045cfa4c7f5e670cdec7b4389d049caf7247
Abhisakh Sarma <abhisakh@gmail.com>
2025-09-13 15:39:56 +0200

    ascii file updated

--- ascii_art.py
+++ ascii_art.py
@@ -1,78 +1,72 @@
+# ascii_art.py
+
 # ANSI color codes
-CYAN = "\033[96m"
+CYAN = "\033[96m"    # For snowman and frame
+WHITE = "\033[97m"   # For falling snow
 RESET = "\033[0m"
 
 STAGES = [
-     CYAN +"""
-    **********************
-    *   *     ___      * *
-    *    *   /___\\    *  *
-    *  *     (o o)     *  *
-    *    * --( : )-- *    *
-    *  *     ( : )     *  *
-    **********************
-    """ + RESET,
-    CYAN + """
-    **********************
-    *   *     ___      * *
-    *    *   /___\\    *  *
-    *  *     (o o)     *  *
-    *    * --( : )-- *    *
-    *  *     (   )     *  *
-    **********************
-    """ + RESET,
-    CYAN + """
-    **********************
-    *   *     ___      * *
-    *    *   /___\\    *  *
-    *  *     (o o)     *  *
-    *    * --(   )-- *    *
-    *  *     (   )     *  *
-    **********************
-    """ + RESET,
-     CYAN + """
-    **********************
-    *   *     ___      * *
-    *    *   /___\\    *  *
-    *  *     (o o)     *  *
-    *    *     )      *   *
-    *  *      )        *  *
-    **********************
-    """ + RESET,
-    CYAN + """
-    **********************
-    *   *     ___      * *
-    *    *   /___\\    *  *
-    *  *     (o o)     *  *
-    *                      *
-    *  *      )        *  *
-    **********************
-    """ + RESET,
-    CYAN + """
-    **********************
-    *   *     ___      * *
-    *    *   /___\\    *  *
-    *  *     (x x)     *  *
-    *                      *
-    *                      *
-    **********************
-    """ + RESET,
-    CYAN + """
-    **********************
-    *   *     ___      * *
-    *    *   /___\\    *  *
-    *                      *
-    *                      *
-    *                      *
-    **********************
-    """ + RESET,
-    CYAN + """
-    **********************
-    *   *     ___      * *
-    *    *   /   \\    *  *
-    *                      *
-    *                      *
-    *                      *
-    **********************
-    """ + RESET
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___      " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /___\\    " + WHITE + "*" + CYAN + "  *\n"
+    "    *  " + WHITE + "*" + CYAN + "     (o o)     " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN +  " --( : )-- " + WHITE + "*" + CYAN + "   *\n"
+    "    *  " + WHITE + "*" + CYAN + "     ( : )     " + WHITE + "*" + CYAN + " *\n"
+    "    **********************" + RESET,
+
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___      " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /___\\    " + WHITE + "*" + CYAN + "  *\n"
+    "    *  " + WHITE + "*" + CYAN + "     (o o)     " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + " --( : )-- " + WHITE + "*" + CYAN + "   *\n"
+    "    *  " + WHITE + "*" + CYAN + "     (   )     " + WHITE + "*" + CYAN + " *\n"
+    "    **********************" + RESET,
+
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___      " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /___\\    " + WHITE + "*" + CYAN + "  *\n"
+    "    *  " + WHITE + "*" + CYAN + "     (o o)     " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + " --(   )-- " + WHITE + "*" + CYAN + "   *\n"
+    "    *  " + WHITE + "*" + CYAN + "     (   )     " + WHITE + "*" + CYAN + " *\n"
+    "    **********************" + RESET,
+
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___      " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /___\\    " + WHITE + "*" + CYAN + "  *\n"
+    "    *  " + WHITE + "*" + CYAN + "     (o o)     " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "     )      " + WHITE + "*" + CYAN + "  *\n"
+    "    *  " + WHITE + "*" + CYAN + "      )        " + WHITE + "*" + CYAN + " *\n"
+    "    **********************" + RESET,
+
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___      " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /___\\    " + WHITE + "*" + CYAN + "  *\n"
+    "    *  " + WHITE + "*" + CYAN + "     (o o)    " + WHITE + "*" + CYAN +  "  *\n"
+    "    *  " + WHITE + "*"          + "            *   "   + CYAN + " *\n"
+    "    *  " + WHITE + "*" + CYAN + "      )        " + WHITE + "*" + CYAN + " *\n"
+    "    **********************" + RESET,
+
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___      " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /___\\    " + WHITE + "*  " + CYAN + "*\n"
+    "    *  " + WHITE + "*" + CYAN + "     (x x)     " + WHITE + "*" + CYAN + " *\n"
+    "    *   " + WHITE + "*"                + "            *  "  + CYAN + " *\n"
+    "    *  " + WHITE + "*"               + "               *" + CYAN + " *\n"
+    "    **********************" + RESET,
+
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___      " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /___\\    " + WHITE + "*" + CYAN + "  *\n"
+    "    *    " + WHITE +  "*"           + "          *   "  + CYAN + " *\n"
+    "    *     " + WHITE +  "*          "     +   "*   "  + CYAN + "*\n"
+    "    *    " + WHITE +  "*"               + "           *  "  + CYAN +   " *\n"
+    "    **********************" + RESET,
+
+    CYAN + "    " + WHITE + "*" + CYAN + "********************" + WHITE + "*" + CYAN + "\n"
+    "    *   " + WHITE + "*" + CYAN + "     ___     " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE + "*" + CYAN + "   /   \\    " + WHITE + "*" + CYAN + " *\n"
+    "    *    " + WHITE +  "*"  + '            '+ "*"  + CYAN + " *\n"
+    "    *     " + WHITE +  "*" + '          '+   "*"    + CYAN + "  *\n"
+    "    *    " + WHITE +  "*"   +'' + '            '+ "*"  + CYAN +   " *\n"
+    "    **********************" + RESET,
 ]
